f00e495cf3814f16dad64ef9485a54d0a7b1c453
balqisrosa <balqisrosasekamayang@gmail.com>
2024-07-14 14:07:08 +0700

    new

--- model/order/order.go
+++ model/order/order.go
@@ -3,6 +3,9 @@ package order
 type Order struct {
 	OrderId int `json:"order_id"`
 	UserId int `json:"user_id"`
+	ProductId int `json:"product_id"`
+	Amount int `json:"amount"`
+	Price float64 `json:"price"`
 	Total float64 `json:"total"`
 	Status string `json:"status"`
 }
